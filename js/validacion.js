3328e83eb79bf80f4965fdbaa453a2de0bf94b6d
Gulnara <gulnaranocetti@gmail.com>
2024-08-16 10:51:16 -0300

    control de campos vacios

--- js/validacion.js
+++ js/validacion.js
@@ -5,3 +5,20 @@ function showAlertSuccess() {
 function showAlertError() {
     document.getElementById("alert-danger").classList.add("show");
 }
+
+// Manejador de eventos para el botón "Registrarme"
+document.getElementById('regBtn').addEventListener('click', function() {
+    // Obtener valores de los campos
+    const nombre = document.getElementById('nombre').value.trim();
+    const email = document.getElementById('email').value.trim();
+    const password = document.getElementById('password1').value.trim();
+    const repetirPassword = document.getElementById('password2').value.trim();
+    const apellido = document.getElementById('apellido').value.trim();
+    const terminos = document.getElementById('terminos').checked;
+
+    // Validaciones
+    if (nombre === '' || email === '' || password === '' || repetirPassword === '' || apellido === '') {
+        showAlertError(); // Campos vacíos
+        return;
+    }
+})
